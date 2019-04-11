9ac39756c626ec91d46901b610f891eb91a19460
Eric Suescun <edsuescun@gmail.com>
2019-04-11 16:47:39 -0500

    Publicacion desde Index exitosa

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,18 @@ class PostsController < ApplicationController
 	def new
 		@post = Post.new
 	end
+
+	def create
+		@post = Post.new(post_params)
+		if @post.save
+			redirect_to posts_path, notice: "Publicación exitosa!"
+		else
+			render :new		#If there is  no success in the access to de DB, we shall return to the form again
+		end
+	end
+
+	private
+		def post_params
+			params.require(:post).permit(:title, :content)
+		end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/posts', to: 'posts#index'
-  get '/posts/new', to: 'posts#new'
+  get '/posts/new', to: 'posts#new', as: 'new_post'
+  post '/posts', to: 'posts#create'
 end
