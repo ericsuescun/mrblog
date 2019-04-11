fc507005190d33df0c0fbc12dd71bc9557608411
Eric Suescun <edsuescun@gmail.com>
2019-04-11 17:53:54 -0500

    Se implementa el borrado en las rutas, controller y vista

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,6 +34,13 @@ class PostsController < ApplicationController
 	  	end
 	end
 
+	def destroy
+	  post = Post.find(params[:id])
+	  post.destroy
+
+	  redirect_to posts_path, notice: "El post fue eliminado!"
+	end
+
 	private
 		def post_params
 			params.require(:post).permit(:title, :content)
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   get '/posts/:id', to: 'posts#show', as: 'post'
   get '/posts/:id/edit', to: 'posts#edit', as: 'edit_post'
   patch '/posts/:id', to: 'posts#update'
+  delete '/posts/:id', to: 'posts#destroy'
 end
