d5d852962cb450b8300130f7c125816ac2a745a5
Eric Suescun <edsuescun@gmail.com>
2019-04-11 16:23:26 -0500

    Se implementa la forma y la accion para crear un post

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'posts', to: 'posts#index'
+  get '/posts', to: 'posts#index'
+  get '/posts/new', to: 'posts#new'
 end
