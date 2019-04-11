f30f20e83d3ac44f3ff19348ebde022ace99e13a
Eric Suescun <edsuescun@gmail.com>
2019-04-11 17:48:52 -0500

    Se implementan otras funciones que faciliten la edicion

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   get '/posts/new', to: 'posts#new', as: 'new_post'
   post '/posts', to: 'posts#create'
   get '/posts/:id', to: 'posts#show', as: 'post'
-  get '/posts/:id/edit', to: 'posts#edit'
+  get '/posts/:id/edit', to: 'posts#edit', as: 'edit_post'
   patch '/posts/:id', to: 'posts#update'
 end
