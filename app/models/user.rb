662456a362efda49cf79efc9b12aee5e0ae45251
Eric Suescun <edsuescun@gmail.com>
2019-04-11 19:53:30 -0500

    Se agregan validaciones al modelo User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
 	has_secure_password validations: false 	#This is because is gonna be encrypted
+
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+	validates :email, presence: true, length: { maximum: 255}, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+
+	validates :password, presence: true, on: :create
+	validates :password, length: { in: 6..20 }, allow_nil: true
+	validates :name, presence: true
 end
