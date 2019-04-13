3f1fd712164eeac410d143bf1f6d0e704f15d82d
Eric Suescun <edsuescun@gmail.com>
2019-04-12 19:34:44 -0500

    Se arregla un problema con la accion destroy, antes no usaba variable de instancia, ahora si porque en la vista se requiere saber si el usuario esta logeado o no

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -38,8 +38,8 @@ class PostsController < ApplicationController
 	end
 
 	def destroy
-	  post = Post.find(params[:id])
-	  post.destroy
+	  @post = Post.find(params[:id])
+	  @post.destroy
 
 	  redirect_to posts_path, notice: "El post fue eliminado!"
 	end
